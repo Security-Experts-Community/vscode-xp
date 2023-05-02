c8d4141403eeb963f81a831f83670f9deee60d69
dmitry <redteamlife@gmail.com>
2023-05-02 12:53:15 +0300

    Выпущен релиз 3.3.1

--- client/src/models/tests/enrichmentUnitTest.ts
+++ client/src/models/tests/enrichmentUnitTest.ts
@@ -1,13 +1,3 @@
-import * as path from "path";
-import * as fs from 'fs';
-
-import { UnitTestContentEditorViewProvider } from '../../views/unitTestEditor/unitTestEditorViewProvider';
-import { BaseUnitTest } from './baseUnitTest';
-import { FileSystemHelper } from '../../helpers/fileSystemHelper';
-import { RuleBaseItem } from '../content/ruleBaseItem';
-import { Correlation } from '../content/correlation';
-import { XpException } from '../xpException';
-import { Enrichment } from '../content/enrichment';
 import { CorrelationUnitTest } from './correlationUnitTest';
 
 export class EnrichmentUnitTest extends CorrelationUnitTest {
