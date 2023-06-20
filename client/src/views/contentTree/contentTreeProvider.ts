dcf9ec4fe0dd2b33b684adfc67697eadb6f86558
dmitry <redteamlife@gmail.com>
2023-06-20 14:01:50 +0300

    - исправлена тихая ошибка, возникающая при клике на директории в дереве контента; - актуализирован changelog.

--- client/src/views/contentTree/contentTreeProvider.ts
+++ client/src/views/contentTree/contentTreeProvider.ts
@@ -100,14 +100,15 @@ export class ContentTreeProvider implements vscode.TreeDataProvider<KbTreeBaseIt
 					} 
 	
 					ContentTreeProvider.setSelectedItem(item);
-	
-					// Открываем код правила
-					const elementUri = vscode.Uri.file(ruleFilePath);
-					const contentDocument = await vscode.workspace.openTextDocument(elementUri);
-					await vscode.window.showTextDocument(contentDocument, vscode.ViewColumn.One);
-					
-					await vscode.commands.executeCommand(UnitTestsListViewProvider.refreshCommand);
 
+					// Открываем код правила
+					if(ruleFilePath) {
+						const elementUri = vscode.Uri.file(ruleFilePath);
+						const contentDocument = await vscode.workspace.openTextDocument(elementUri);
+						await vscode.window.showTextDocument(contentDocument, vscode.ViewColumn.One);
+						await vscode.commands.executeCommand(UnitTestsListViewProvider.refreshCommand);
+					}
+	
 					// Выделяем только что созданное правило.
 					await contentTree.reveal(
 						item, 
