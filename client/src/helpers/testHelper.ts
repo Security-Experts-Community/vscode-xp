72f2d846e3a69205de3ca2550b5dd63b44e03a14
dmitry <redteamlife@gmail.com>
2023-07-30 17:12:06 +0300

    Заработало добавление тестов с подсветкой синтаксиса.

--- client/src/helpers/testHelper.ts
+++ client/src/helpers/testHelper.ts
@@ -424,7 +424,7 @@ export class TestHelper {
 
 			// Из textarea новые строки только \n, поэтому надо их поправить под систему.
 			rawEvents = rawEvents.replace(/(?<!\\)\n/gm, EOL);
-			test.setRawEvents(rawEvents);
+			test.setRawEvents(TestHelper.compressTestCode(rawEvents));
 
 			// Код теста.
 			let testCode = plainTest?.testCode;
--- client/src/views/integrationTests/integrationTestEditorViewProvider.ts
+++ client/src/views/integrationTests/integrationTestEditorViewProvider.ts
@@ -55,6 +55,7 @@ export class IntegrationTestEditorViewProvider  {
 					// Обновляем интеграционные тесты для того, чтобы можно было увидеть актуальные тесты при их модификации на ЖД.
 					if(!rule) {
 						ExtensionHelper.showUserError("Ошибка открытия интеграционных тестов.");
+						return;
 					}
 					rule.reloadIntegrationalTests();
 					return provider.showEditor(rule);
@@ -150,6 +151,7 @@ export class IntegrationTestEditorViewProvider  {
 				intergrationalTest.forEach(it => {
 					const jsonedTestObject = JSON.stringify(it);
 
+					const formattedRawEvents = TestHelper.formatTestCodeAndEvents(it.getRawEvents());
 					const formattedTestCode = TestHelper.formatTestCodeAndEvents(it.getTestCode());
 					const formattedNormalizedEvents = TestHelper.formatTestCodeAndEvents(it.getNormalizedEvents());
 
@@ -172,7 +174,7 @@ export class IntegrationTestEditorViewProvider  {
 
 					plain["IntegrationalTests"].push({
 						"TestNumber" : it.getNumber(),
-						"RawEvents" : it.getRawEvents(),
+						"RawEvents" : formattedRawEvents,
 						"NormEvents" : formattedNormalizedEvents,
 						"TestCode" : formattedTestCode,
 						"TestOutput" : it.getOutput(),
@@ -276,8 +278,14 @@ export class IntegrationTestEditorViewProvider  {
 					}
 	
 					// Актуализируем сырые события в тесте из вьюшки.
-					const rawEvents = message.rawEvents;
+					let rawEvents = message.rawEvents;
+					if(!rawEvents) {
+						ExtensionHelper.showUserInfo("Не заданы сырые события для нормализации. Задайте события и повторите.");
+						return;
+					}
+
 					const currTest = IntegrationTest.convertFromObject(message.test);
+					rawEvents = TestHelper.compressTestCode(rawEvents);
 					currTest.setRawEvents(rawEvents);
 					await currTest.save();
 	
