0e36426466adbc30f5b77dbda55cd89ca67da788
Mikhail Stanishevsky <50733333+BDOXHN@users.noreply.github.com>
2023-09-04 19:13:37 +0700

    Добавлен флексбокс, предотвращающий разъезд разметки при проверке локализаций (#136)
    Co-authored-by: Mikhail Stanishevskiy <mstanishevskiy@ptsecurity.com>

--- client/src/views/localizationEditor/localizationEditorViewProvider.ts
+++ client/src/views/localizationEditor/localizationEditorViewProvider.ts
@@ -18,14 +18,14 @@ import { IntegrationTestRunner, IntegrationTestRunnerOptions } from '../../model
 import { RunIntegrationTestDialog } from '../runIntegrationDialog';
 import { Enrichment } from '../../models/content/enrichment';
 
-export class LocalizationEditorViewProvider  {
+export class LocalizationEditorViewProvider {
 
 	public static readonly viewId = 'LocalizationView';
-	
+
 	private _view?: vscode.WebviewPanel;
 	private _rule: RuleBaseItem;
 
-	constructor(		
+	constructor(
 		private readonly _config: Configuration,
 		private readonly _templatePath: string
 	) { }
@@ -36,20 +36,20 @@ export class LocalizationEditorViewProvider  {
 			Configuration.get().getExtensionPath(), "client", "templates", "LocalizationEditor.html");
 
 		const provider = new LocalizationEditorViewProvider(config, templateFilePath);
-	
+
 		config.getContext().subscriptions.push(
 			vscode.commands.registerCommand(
-				LocalizationEditorViewProvider.showLocalizationEditorCommand, 
+				LocalizationEditorViewProvider.showLocalizationEditorCommand,
 				async (correlation: Correlation) => provider.showLocalizationEditor(correlation)
 			)
-		);	
+		);
 	}
 
 	public static showLocalizationEditorCommand = "LocalizationView.showLocalizationEditor";
-	public async showLocalizationEditor(rule: RuleBaseItem)  {
+	public async showLocalizationEditor(rule: RuleBaseItem) {
 
 		// Если открыта еще одна локализация, то закрываем её перед открытием новой.
-		if(this._view) {
+		if (this._view) {
 			this._view.dispose();
 			this._view = undefined;
 		}
@@ -60,39 +60,39 @@ export class LocalizationEditorViewProvider  {
 			const localizations = rule.getLocalizations();
 
 			const plainLocalizations: any[] = [];
-			localizations.forEach( loc => {
+			localizations.forEach(loc => {
 
 				const locId = loc.getLocalizationId();
-				if(!locId) {
+				if (!locId) {
 					throw new XpException("Не задан LocalizationId.");
 				}
 
 				const criteria = loc.getCriteria();
-				if(!criteria) {
+				if (!criteria) {
 					throw new XpException(`Критерий для правила локализации не задан: LocalizationId = '${locId}'.`);
 				}
 
 				// Ошибка в том случае, если нет обоих локализаций.
-				if(!loc.getRuLocalizationText() && !loc.getEnLocalizationText()) {
-					throw new XpException(`Для критерия LocalizationId = '${locId}' не задано ни одного значения.`);	
+				if (!loc.getRuLocalizationText() && !loc.getEnLocalizationText()) {
+					throw new XpException(`Для критерия LocalizationId = '${locId}' не задано ни одного значения.`);
 				}
 
 				let ruLocalizationText = loc.getRuLocalizationText();
-				if(!loc.getRuLocalizationText()) {
+				if (!loc.getRuLocalizationText()) {
 					ruLocalizationText = "";
 				}
 
 				let enLocalizationText = loc.getEnLocalizationText();
-				if(!enLocalizationText) {
+				if (!enLocalizationText) {
 					enLocalizationText = "";
-				} 
-				
-				plainLocalizations.push({
-					"Criteria" : criteria,
+				}
 
-					"LocalizationId" : locId,
-					"RuLocalization" : ruLocalizationText,
-					"EnLocalization" : enLocalizationText,
+				plainLocalizations.push({
+					"Criteria": criteria,
+
+					"LocalizationId": locId,
+					"RuLocalization": ruLocalizationText,
+					"EnLocalization": enLocalizationText,
 				});
 			});
 
@@ -101,12 +101,12 @@ export class LocalizationEditorViewProvider  {
 				LocalizationEditorViewProvider.viewId,
 				`Правила локализации '${rule.getName()}'`,
 				vscode.ViewColumn.One,
-				{retainContextWhenHidden : true});
+				{ retainContextWhenHidden: true });
 
 			this._view.webview.options = {
 				enableScripts: true
 			};
-			
+
 			this._view.webview.onDidReceiveMessage(
 				this.receiveMessageFromWebView,
 				this
@@ -115,16 +115,16 @@ export class LocalizationEditorViewProvider  {
 			const config = Configuration.get();
 			const resoucesUri = config.getExtensionUri();
 			const extensionBaseUri = this._view.webview.asWebviewUri(resoucesUri);
-			
+
 			const locExamples = this._rule.getLocalizationExamples();
 			const templatePlainObject = {
-				"RuleName" : rule.getName(),
-				"RuDescription" : rule.getRuDescription(),
-				"EnDescription" : rule.getEnDescription(),
-				"Localizations" : plainLocalizations,
-				"ExtensionBaseUri" : extensionBaseUri,
-				"LocalizationExamples" : locExamples,
-				"IsLocalizableRule" : this.isLocalizableRule(rule),
+				"RuleName": rule.getName(),
+				"RuDescription": rule.getRuDescription(),
+				"EnDescription": rule.getEnDescription(),
+				"Localizations": plainLocalizations,
+				"ExtensionBaseUri": extensionBaseUri,
+				"LocalizationExamples": locExamples,
+				"IsLocalizableRule": this.isLocalizableRule(rule),
 			};
 
 			// Подгружаем шаблон и шаблонизируем данные.
@@ -134,13 +134,13 @@ export class LocalizationEditorViewProvider  {
 
 			this._view.webview.html = htmlContent;
 		}
-		catch(error) {
+		catch (error) {
 			ExceptionHelper.show(error, `Не удалось открыть правила локализации.`);
 		}
 	}
 
-	private isLocalizableRule(rule : RuleBaseItem) : boolean {
-		if(rule instanceof Enrichment) {
+	private isLocalizableRule(rule: RuleBaseItem): boolean {
+		if (rule instanceof Enrichment) {
 			return false;
 		}
 
@@ -152,7 +152,7 @@ export class LocalizationEditorViewProvider  {
 			case 'buildLocalizations': {
 
 				const locExamples = await this.getLocalizationExamples();
-				if(locExamples.length === 0) {
+				if (locExamples.length === 0) {
 					return ExtensionHelper.showUserInfo(
 						"По имеющимся событиям не отработала ни одна локализация. Проверьте, что интеграционные тесты проходят, корректны критерии локализации. После исправлений повторите.");
 				}
@@ -178,33 +178,33 @@ export class LocalizationEditorViewProvider  {
 					this._rule.setEnDescription(enDescription);
 
 					// Получаем нужные данные из вебвью и тримим их.
-					const criteria = (localization.Criteria as string []).map(c => c.trim());
-					const ruLocalizations = (localization.RuLocalizations as string []).map(c => StringHelper.textToOneLineAndTrim(c));
-					const enLocalizations = (localization.EnLocalizations as string []).map(c => StringHelper.textToOneLineAndTrim(c));
-					const localizationIds = (localization.LocalizationIds as string []).map(c => c.trim());
+					const criteria = (localization.Criteria as string[]).map(c => c.trim());
+					const ruLocalizations = (localization.RuLocalizations as string[]).map(c => StringHelper.textToOneLineAndTrim(c));
+					const enLocalizations = (localization.EnLocalizations as string[]).map(c => StringHelper.textToOneLineAndTrim(c));
+					const localizationIds = (localization.LocalizationIds as string[]).map(c => c.trim());
 
 					const firstDuplicate = this.findDuplicates(criteria);
-					if(firstDuplicate != null) {
+					if (firstDuplicate != null) {
 						ExtensionHelper.showUserError(`Критерий '${firstDuplicate}' дублируется в нескольких правилах локализации`);
 						return;
 					}
 
 					// Преобразуем полученные данные в нужный формат.
-					const localizations = criteria.map( (cr, index) => {
+					const localizations = criteria.map((cr, index) => {
 						const ruLoc = ruLocalizations[index];
 						const enLoc = enLocalizations[index];
 						const loc = Localization.create(cr, ruLoc, enLoc);
 
 						const locId = localizationIds[index];
-						if(locId) {
+						if (locId) {
 							loc.setLocalizationId(locId);
 						}
-						
+
 						return loc;
 					});
 
 					// Обновляем локализации и сохраняем их.
-					if(localizations.length !== 0) {
+					if (localizations.length !== 0) {
 						this._rule.setLocalizationTemplates(localizations);
 					}
 
@@ -218,28 +218,28 @@ export class LocalizationEditorViewProvider  {
 		}
 	}
 
-	private async getLocalizationExamples() : Promise<LocalizationExample[]> {
+	private async getLocalizationExamples(): Promise<LocalizationExample[]> {
 		return await vscode.window.withProgress({
 			location: vscode.ProgressLocation.Notification,
 			cancellable: true,
 		}, async (progress, token) => {
 			try {
-				progress.report({message : `Получение зависимостей правила для корректной сборки графа корреляций.`});
+				progress.report({ message: `Получение зависимостей правила для корректной сборки графа корреляций.` });
 				const ritd = new RunIntegrationTestDialog(this._config);
 				const options = await ritd.getIntegrationTestRunOptions(this._rule);
 				options.cancellationToken = token;
 
-				progress.report({message : `Получение корреляционных событий на основе интеграционных тестов правила.`});
+				progress.report({ message: `Получение корреляционных событий на основе интеграционных тестов правила.` });
 				const outputParser = new SiemJOutputParser();
 				const testRunner = new IntegrationTestRunner(this._config, outputParser);
 				const siemjResult = await testRunner.run(this._rule, options);
 
-				if(!siemjResult.testsStatus) {
+				if (!siemjResult.testsStatus) {
 					throw new XpException("Не все интеграционные тесты прошли.");
 				}
 
-				progress.report({message : `Генерация локализаций на основе корреляционных событий из интеграционных тестов.`});
-				const siemjManager = new SiemjManager(this._config); 
+				progress.report({ message: `Генерация локализаций на основе корреляционных событий из интеграционных тестов.` });
+				const siemjManager = new SiemjManager(this._config);
 				const locExamples = await siemjManager.buildLocalizationExamples(this._rule, siemjResult.tmpDirectoryPath);
 				return locExamples;
 			}
@@ -249,8 +249,8 @@ export class LocalizationEditorViewProvider  {
 		});
 	}
 
-	private findDuplicates(arr) : string {
-		const sorted_arr = arr.slice().sort(); 
+	private findDuplicates(arr): string {
+		const sorted_arr = arr.slice().sort();
 		for (let i = 0; i < sorted_arr.length - 1; i++) {
 			if (sorted_arr[i + 1] == sorted_arr[i]) {
 				return sorted_arr[i];
