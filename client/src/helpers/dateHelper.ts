4732f81a49afb759d2d07eb989d77874072674a3
dmitry <redteamlife@gmail.com>
2023-10-27 10:08:41 +0300

    поправил import и убрал лишнюю зависимость

--- client/src/helpers/dateHelper.ts
+++ client/src/helpers/dateHelper.ts
@@ -1,4 +1,4 @@
-import moment = require('moment');
+import * as moment from 'moment';
 
 export class DateHelper {
 	public static dateToString(date: Date): string {
--- client/src/views/retroCorrelation/correlateEventsCommand.ts
+++ client/src/views/retroCorrelation/correlateEventsCommand.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as os from 'os';
 import * as fs from 'fs';
 import * as path from 'path';
-import moment = require('moment');
+import * as moment from 'moment';
 
 import { RegExpHelper } from '../../helpers/regExpHelper';
 import { Configuration } from '../../models/configuration';
